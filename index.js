7cc48f66cfc860b7af236b2d1199232afdac56e7
adza390 <137942425+adza390@users.noreply.github.com>
2025-05-27 22:22:28 +0200

    Promijenjen index.js file

--- index.js
+++ index.js
@@ -1,12 +1,21 @@
 const { Client, LocalAuth } = require('whatsapp-web.js');
-const qrcode = require('qrcode-terminal');
+const qrcode = require('qrcode');  // koristimo 'qrcode' umjesto 'qrcode-terminal'
+const fs = require('fs');
 const handleMessage = require('./commands');
 
 const client = new Client({
     authStrategy: new LocalAuth()
 });
 
-client.on('qr', qr => qrcode.generate(qr, { small: true }));
+client.on('qr', async (qr) => {
+    try {
+        await qrcode.toFile('qr-code.png', qr);
+        console.log('QR kod je generisan i sačuvan kao qr-code.png');
+    } catch (err) {
+        console.error('Greška prilikom generisanja QR koda:', err);
+    }
+});
+
 client.on('ready', () => console.log('Bot je spreman.'));
 client.on('message', msg => handleMessage(client, msg));
 
